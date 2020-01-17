fe704adab54369d32bf01260a107ff5ea33fbc48
gali zorea <galizorea@Gali-MacBook-Pro.local>
2020-01-17 23:46:09 +0200

    Layout styling
    1. Added a new css file to handle the various styling of the elements
    2. Added functionaliity to display/hide the side bar
    3. Reformatted the styling of the feature items

--- search.js
+++ search.js
@@ -7,12 +7,14 @@
   });
 
   var features = ''
-  map.once("idle", function(e) {
-    
-    features = map.queryRenderedFeatures({layers: ['vegan-places']});
+  map.once("idle", function (e) {
+
+    features = map.queryRenderedFeatures({
+      layers: ['vegan-places']
+    });
     renderListings(features);
 
-});
+  });
 
   /*  var popup = new mapboxgl.Popup({
     closeButton: false,  offset: [0, -15] 
@@ -21,71 +23,81 @@
   var filterEl = document.getElementById('feature-filter');
   var listingEl = document.getElementById('feature-listing');
 
-  filterEl.addEventListener('keyup', function(e) {
-   // var features = map.queryRenderedFeatures({layers: ['vegan-places']});
+  filterEl.addEventListener('keyup', function (e) {
 
     var value = e.target.value;
-     
-    // Filter visible features that don't match the input value.
-    var filtered = features.filter(function(feature) {
-    var name = feature.properties.title;
-    return name.indexOf(value) === 0;
-    });
-     
-   renderListings(filtered);
-     
-    // Set the filter to populate features into the layer.
-    map.setFilter('vegan-places', [
-    'match',
-    ['get', 'title'],
-    filtered.map(function(feature) {
-    return feature.properties.title;
-    }),
-    true,
-    false
-    ]);
-});
 
-function renderListings(features) {
+    var filtered = features.filter(function (feature) {
+      var name = feature.properties.title;
+      return name.indexOf(value) === 0;
+    });
+
+    renderListings(filtered);
+
+    map.setFilter('vegan-places', [
+      'match',
+      ['get', 'title'],
+      filtered.map(function (feature) {
+        return feature.properties.title;
+      }),
+      true,
+      false
+    ]);
+  });
+
+  function renderListings(features) {
     listingEl.innerHTML = '';
     if (features.length) {
-    features.forEach(function(feature) {
-    var prop = feature.properties;
-    var item = document.createElement('a');
-    item.textContent = prop.title + ' (' + prop.description + ')';
-  /* item.addEventListener('mouseover', function() {
-    // Highlight corresponding feature on the map
-    popup
-    .setLngLat(feature.geometry.coordinates)
-    .setText(
-    feature.properties.title +
-    ' (' +
-    feature.properties.description +
-    ')'
-    )
-    .addTo(map);
-    });*/
-    listingEl.appendChild(item);
+      features.forEach(function (feature) {
+        var prop = feature.properties;
+        var item = document.createElement('li');
+        item.className = "listing-item";
+        item.textContent = prop.title + ' (' + prop.description + ')';
+        /* item.addEventListener('mouseover', function() {
+          // Highlight corresponding feature on the map
+          popup
+          .setLngLat(feature.geometry.coordinates)
+          .setText(
+          feature.properties.title +
+          ' (' +
+          feature.properties.description +
+          ')'
+          )
+          .addTo(map);
+          });*/
+        listingEl.appendChild(item);
+      });
+
+      // Show the filter input
+      filterEl.parentNode.style.display = 'block';
+    }
+  }
+
+  map.on('click', function (e) {
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ['vegan-places'] // replace this with the name of the layer
     });
-     
-    // Show the filter input
-    filterEl.parentNode.style.display = 'block';
-    } 
+
+    if (!features.length) {
+      return;
     }
 
-    map.on('click', function(e) {
-        var features = map.queryRenderedFeatures(e.point, {
-          layers: ['vegan-places'] // replace this with the name of the layer
-        });
-      
-        if (!features.length) {
-          return;
-        }
-      
-        var feature = features[0];
-      
-        var popup = new mapboxgl.Popup({ offset: [0, -15] })
-          .setLngLat(feature.geometry.coordinates)
-          .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
-          .addTo(map);
-      });
+    var feature = features[0];
+
+    var popup = new mapboxgl.Popup({
+        offset: [0, -15]
+      })
+      .setLngLat(feature.geometry.coordinates)
+      .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+      .addTo(map);
+  });
+
+  function expand_sidebar() {
+    if (document.getElementById("sidebar").style.display == "none") {
+      document.getElementById("sidebar").style.display = "block";
+      document.getElementsByClassName("sidebar-btn")[0].innerHTML = '✕';
+    } else {
+      document.getElementById("sidebar").style.display = "none";
+      document.getElementsByClassName("sidebar-btn")[0].innerHTML = '☰';
+    }
+  }
